91336e0a43b1393ac71029f5a318464055ff1ac2
Justin Piotroski <justin.piotroski@gmail.com>
2020-05-17 17:41:27 -0700

    Added the BarCode component

--- lib/inkblot/components.rb
+++ lib/inkblot/components.rb
@@ -1,5 +1,6 @@
 require 'inkblot/components/component'
 require 'inkblot/components/simple_text'
+require 'inkblot/components/bar_code'
 require 'inkblot/components/qr_code'
 require 'inkblot/components/full_screen_image'
 require 'inkblot/components/table_list'
---
+++ lib/inkblot/components/bar_code.rb
@@ -0,0 +1,55 @@
+require 'barby/barcode/ean_13'
+require 'barby/outputter/png_outputter'
+
+require_relative 'helpers/data_url'
+
+module Inkblot
+  module Components
+    # For generating 1D UPC style codes
+    class BarCode < Component
+      include Helpers::DataUrl
+
+      # Sugar method for the code contents
+      def code
+        options[:code]
+      end
+
+      private
+
+      # Handles sizing and margins, and creates the data url
+      def computed
+        dta = OpenStruct.new
+
+        get_width(dta)
+        get_height(dta)
+
+        dta.margin_top = options.fetch(:margin_top, 0)
+        dta.margin_left = options.fetch(:margin_left, 0)
+
+        dta.margins = if dta.margin_top.zero? && dta.margin_left.zero?
+                        ''
+                      else
+                        m = +' '
+                        if dta.margin_top
+                          m << "margin-top: #{dta.margin_top}%; "
+                        end
+
+                        if dta.margin_left
+                          m << "margin-left: #{dta.margin_left}%;"
+                        end
+
+                        m
+                      end
+
+        dta.data_url = data_url_from_binary(generate_code.to_png(height: 60))
+
+        dta.to_h
+      end
+
+      # Uses Barby to encode the code as an EAN-13
+      def generate_code
+        Barby::EAN13.new(code)
+      end
+    end
+  end
+end
