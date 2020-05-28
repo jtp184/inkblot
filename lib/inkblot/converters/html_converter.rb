20423efc4b683edddf54e9b72a04cfa8e5c23e3f
Justin Piotroski <justin.piotroski@gmail.com>
2020-05-28 04:30:51 -0700

    Refactor the html converter based on Grover

--- lib/inkblot/converters/html_converter.rb
+++ lib/inkblot/converters/html_converter.rb
@@ -4,24 +4,21 @@ module Inkblot
   module Converters
     # Converts HTML input into an image by passing it into an ImageConverter
     class HtmlConverter < Converter
-      # Uses Grover to create a page from passed input.
+      # Uses image_contents to create a page from passed input.
       # Returns a (closed) tempfile created from an ImageConverter
       def convert
-        ImageConverter.new(
-          input: Grover.new(input, viewport: Display.size).to_png
-        ).convert
+        ImageConverter.new(input: image_contents).convert
       end
 
-      # Reads the tempfile and returns the contents. 
-      # Converts if this has not been done
+      # Returns png data from a grover instance called on input
       def image_contents
-        convert! unless output
-        File.read(output.path)
+        Grover.new(input, viewport: Display.size).to_png
       end
 
-      # Saves the image permanently to path. Converts if this has not been done
+      # Saves the converted image permanently to path. Converts if this has not been done
       def save(path)
-        convert! unless output
+        convert!
+        
         File.open(path, 'w+b') do |f|
           f << File.read(output.path)
         end
