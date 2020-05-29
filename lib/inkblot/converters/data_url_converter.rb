7a31388ff5706c250385d8921c05865c791a81ac
Justin Piotroski <justin.piotroski@gmail.com>
2020-05-29 01:45:07 -0700

    Refactor DataUrlConverter to guess at format and filetype when not provided

--- lib/inkblot/converters/data_url_converter.rb
+++ lib/inkblot/converters/data_url_converter.rb
@@ -6,12 +6,14 @@ module Inkblot
     class DataUrlConverter < Converter
       # Possible source formats
       FORMATS = %i[path file binary base64].freeze
+      # Matches base64 strings for guessing
+      BASE64_MATCHER = %r'[^-A-Za-z0-9+/=]|=[^=]|={3,}$'
 
       # Takes in additional +opts+ for filetype and base64
       def initialize(opts = {})
         super
-        @filetype = opts.fetch(:filetype, 'png')
-        @format = opts.fetch(:format, :binary)
+        @format = opts.fetch(:format, infer_format)
+        @filetype = opts.fetch(:filetype, infer_filetype)
       end
 
       # Converts the input using data_url_from_binary
@@ -20,6 +22,35 @@ module Inkblot
       end
 
       private
+
+      # Guess the input format
+      def infer_format
+        izza = ->(ip) { input.is_a?(ip) }
+
+        if izza[File] || izza[Tempfile]
+          :file
+        elsif izza[String] && input.ascii_only? && Pathname.new(input).exist?
+          :path
+        elsif izza[String] && input.match?(BASE64_MATCHER)
+          :base64
+        else
+          :binary
+        end
+      end
+
+      # Infers filetype based on format
+      def infer_filetype
+        case @format
+        when :file
+          File.extname(input.path).gsub(/^\./, '')
+        when :path
+          File.extname(input).gsub(/^\./, '')
+        when :base64
+          input.match(%r|data:image/(\w+);base64|)[1]
+        else
+          'png'
+        end
+      end
 
       # Encodes the input into a base64 data url, encoding if needed
       def data_url_from_binary
