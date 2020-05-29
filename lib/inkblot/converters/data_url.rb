27645a0a41c209f878b5c4f3be554920e043117b
Justin Piotroski <justin.piotroski@gmail.com>
2020-05-28 19:44:47 -0700

    Improved DataUrl converter

--- lib/inkblot/converters/data_url.rb
+++ lib/inkblot/converters/data_url.rb
@@ -2,21 +2,25 @@ require 'base64'
 
 module Inkblot
   module Converters
-      # For handling data url creation for image producing components
-      class DataUrl < Converter
-        def convert
-          data_url_from_binary(input, @filetype || 'png')
-        end
+    # For handling data url creation for image producing components
+    class DataUrl < Converter
+      def initialize(opts = {})
+        super
+        @filetype = opts.fetch(:filetype, 'png')
+      end
 
-        private
+      def convert
+        data_url_from_binary(input, @filetype)
+      end
 
-        # Encodes the binary string +bs+ into a base64 data url
-        def data_url_from_binary(bs, filetype='png')
-          data_url = +"" << 'data:image/'
-          data_url << filetype
-          data_url << ';base64,'
-          data_url << Base64.encode64(bs).gsub("\n", '')
-        end
+      private
+
+      # Encodes the binary string +bs+ into a base64 data url
+      def data_url_from_binary(bs, filetype)
+        data_url = +'' << 'data:image/'
+        data_url << filetype
+        data_url << ';base64,'
+        data_url << Base64.encode64(bs).gsub("\n", '')
       end
     end
   end
