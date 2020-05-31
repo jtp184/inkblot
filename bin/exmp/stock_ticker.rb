cb0567e0c02d56dde883b43b2bb64dea0bcbc4c7
Justin Piotroski <justin.piotroski@gmail.com>
2020-05-30 18:02:06 -0700

    Uncomment for testing

--- bin/exmp/stock_ticker.rb
+++ bin/exmp/stock_ticker.rb
@@ -100,28 +100,26 @@ class StockTicker
   end
 end
 
-@s = StockTicker.new(symbols: %w[AAPL FB])
 
-binding.pry
+#=== For Displaying on the EPD ===##
+Inkblot::Buttons.init unless Inkblot::Buttons.ready?
+@t = StockTicker.new(symbols: ['AAPL', 'FB'])
+# binding.pry
 
-##=== For Displaying on the EPD ===##
-# Inkblot::Buttons.init unless Inkblot::Buttons.ready?
-# @t = StockTicker.new(symbols: ['AAPL', 'FB'])
+refresh_time = 30
 
-# refresh_time = 30
+begin
+  loop do
+    Inkblot::Display.show(@t)
+    Inkblot::Buttons.get_press(refresh_time)
 
-# begin
-#   loop do
-#     Inkblot::Display.show(@t)
-#     Inkblot::Buttons.get_press(refresh_time)
+    @t.refresh
 
-#     @t.refresh
-
-#     puts
-#     puts Time.now, @t.latest_report
-#   end
-# rescue IndexError
-#   Inkblot::Display.clear
-#   Inkblot::Buttons.release
-#   exit
-# end
+    puts
+    puts Time.now, @t.latest_report
+  end
+rescue IndexError
+  Inkblot::Display.clear
+  Inkblot::Buttons.release
+  exit
+end
